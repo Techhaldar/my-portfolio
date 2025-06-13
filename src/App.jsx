d809bb06771ac7734128a8aae771211c2fb3efce
Ratin <ratinhaldar123@gmail.com>
2025-06-13 13:28:56 +0530

    About Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import "./App.css";
 import NavbarMain from "./components/Navbar/NavbarMain";
 import HeroMain from "./components/HeroSection.jsx/HeroMain";
+import SubHeroSection from "./components/HeroSection.jsx/SubHeroSection";
+import AboutMeMain from "./components/AboutMeSection/AboutMeMain";
+
 function App() {
   return (
     <main className="font-body">
       <NavbarMain />
       <HeroMain />
+      <SubHeroSection />
+      <AboutMeMain />
     </main>
   );
 }
---
+++ src/components/AboutMeSection/AboutMeText.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AboutMeText = () => {
+  return (
+    <div className="flex flex-col md:items-start sm:items-center md:text-left">
+      <h2 className="text-6xl text-cyan mb-10">About Me</h2>
+      <p className="text-white sm:text-center md:text-left">
+        I am Ratin Haldar and I am 21 year old computer science graduated (BCA)
+        student. I am looking to secure a challenging position where I can
+        effectively contribute my skills as software professional for growth of
+        organization and myself.
+      </p>
+      <button className="border border-orange rounded-full py-2 px-4 text-lg flex items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white hover:text-cyan">
+        My Projects
+      </button>
+    </div>
+  );
+};
+
+export default AboutMeText;
